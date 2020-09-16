a65dd595458cdb4734de9e1c0cddc689cc88d8ec
ManikantaSatyaSubbareddy <50864579+subbareddykovvuri@users.noreply.github.com>
2020-09-16 17:22:26 +0530

    Update consecutive_ones.c

--- consecutive_ones.c
+++ consecutive_ones.c
@@ -1,7 +1,26 @@
+#include<stdio.h>
+int lon_con_ones(int n){
+	int max=0;
+	while(n>0){
+		if(n&1==1){
+			max=max+1;
+		}
+		
+	}
+}
+int main(){
+	int n,l;
+	scanf("%d",&n);
+	l=lon_con_ones(n);
+	printf("%d",l);
+}
+
 /*
 longest con ones
 
 
 93
 1011101
+
+3
 */
